5121a605c61f635185ad34410331bcc48c772718
Edwin-9603 <egc4611@gmail.com>
2022-09-13 10:49:42 -0400

    new updates

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-'''Flask WSGI for multiple routes'''
-
-
+"""This module starts a Flask web application and displays text starting with C
+"""
 from flask import Flask
+from flask import escape
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Returns the string Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
-    '''First function. Prints on /'''
-    return "Hello HBNB!"
+    """Returns the string HBNB"""
+    return 'HBNB'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def just_hbnb():
-    '''Second function. Prints on /hbnb'''
-    return "HBNB"
-
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """Returns a string starting with C"""
+    mod_text = escape(text)
+    return 'C %s' % mod_text.replace("_", " ")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
