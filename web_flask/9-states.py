f35ad2f0aac2cf332702ad7dfc7d3eda9b85cf07
Edwin-9603 <egc4611@gmail.com>
2022-09-14 18:11:03 -0400

    New updates

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application and displays an integer in HTML
+"""
+from flask import Flask
+from flask import escape
+from flask import render_template
+from models import storage
+from models.state import State
+from os import getenv
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def display_cities_by_state():
+    """displays cities by state"""
+    state_dict = storage.all(State)
+    return render_template('8-cities_by_states.html', state_dict=state_dict)
+
+@app.route('/states/<id>', strict_slashes=False)
+def display_cities_by_state():
+    """displays cities by state"""
+    state_dict = storage.all(State)
+    return render_template('8-cities_by_states.html', state_dict=state_dict)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
