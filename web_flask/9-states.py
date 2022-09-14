feed8edc9400a62d42604abd97d767e30b8e82ba
Edwin-9603 <egc4611@gmail.com>
2022-09-14 18:16:39 -0400

    new updates

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-"""This module starts a Flask web application and displays an integer in HTML
+"""This module starts a Flask web application state and cities by state id
 """
 from flask import Flask
 from flask import escape
 from flask import render_template
 from models import storage
-from models.state import State
-from os import getenv
+from models import State
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def display_cities_by_state():
-    """displays cities by state"""
-    state_dict = storage.all(State)
-    return render_template('8-cities_by_states.html', state_dict=state_dict)
-
 @app.route('/states/<id>', strict_slashes=False)
-def display_cities_by_state():
-    """displays cities by state"""
+def display_states_and_cities(id=None):
+    """displays states and cities by state id"""
     state_dict = storage.all(State)
-    return render_template('8-cities_by_states.html', state_dict=state_dict)
+    found = None
+    if id is not None:
+        for state in state_dict.values():
+            if state.id == id:
+                found = state
+
+    return render_template('9-states.html', state_dict=state_dict, id=id,
+                           found=found)
 
 
 @app.teardown_appcontext
 def teardown_db(self):
     """removes the current SQLAlchemy Session"""
     storage.close()
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
